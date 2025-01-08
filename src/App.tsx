b34135c71261c07bebc7272ff0aa279db018f6e0
LeomharT <leo.liao.lzy@gmail.com>
2025-01-08 15:31:46 +0800

    update:accelerate star

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,216 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from 'react';
+import {
+	AxesHelper,
+	Clock,
+	Color,
+	DoubleSide,
+	InstancedMesh,
+	MeshBasicMaterial,
+	Object3D,
+	PerspectiveCamera,
+	PlaneGeometry,
+	Scene,
+	ShaderMaterialParameters,
+	Vector3,
+	WebGLRenderer,
+} from 'three';
+import {
+	EffectComposer,
+	OutputPass,
+	RenderPass,
+	ShaderPass,
+} from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import fragmentShader from './shader/fragment.glsl?raw';
+import vertexShader from './shader/vertex.glsl?raw';
+import classes from './style.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	function random(min: number, max: number) {
+		const diff = Math.random() * (max - min);
+		return min + diff;
+	}
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const initialScene = () => {
+		const el = document.querySelector('#container') as HTMLDivElement;
+
+		/**
+		 * Basic
+		 */
+
+		const renderer = new WebGLRenderer({
+			alpha: true,
+			antialias: true,
+		});
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.setPixelRatio(window.devicePixelRatio);
+		el.append(renderer.domElement);
+
+		const scene = new Scene();
+		scene.background = new Color('#1e1e1e');
+
+		const camera = new PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000
+		);
+		camera.position.set(1, 1, 1);
+		camera.lookAt(scene.position);
+
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.enableDamping = true;
+		controls.enableZoom = true;
+
+		const stats = new Stats();
+		el.append(stats.dom);
+
+		/**
+		 * Effects
+		 */
+
+		const composer = new EffectComposer(renderer);
+
+		const renderPass = new RenderPass(scene, camera);
+		composer.addPass(renderPass);
+
+		const shiftPass = new ShaderPass({
+			uniforms: {
+				tDiffuse: { value: null },
+				opacity: { value: 1.0 },
+			},
+			vertexShader,
+			fragmentShader,
+		} as ShaderMaterialParameters);
+		composer.addPass(shiftPass);
+
+		const outputPass = new OutputPass();
+		composer.addPass(outputPass);
+
+		/**
+		 * Variables
+		 */
+
+		const STAR_COUNT = 20;
+		const STARS: {
+			pos: Vector3;
+			speed: number;
+		}[] = [];
+
+		function resetStar() {
+			const pos = new Vector3(random(1, -1), random(1, -1), random(1, -1));
+			const speed = random(19.5, 42);
+
+			return {
+				pos,
+				speed,
+			};
+		}
+
+		for (let i = 0; i < STAR_COUNT; i++) {
+			STARS.push(resetStar());
+		}
+
+		/**
+		 * Scene
+		 */
+
+		const starGeometry = new PlaneGeometry(0.05, 0.05);
+		const starMaterial = new MeshBasicMaterial({
+			side: DoubleSide,
+			color: 'white',
+		});
+		const star = new InstancedMesh(starGeometry, starMaterial, STAR_COUNT);
+
+		const targetObject = new Object3D();
+
+		for (let i = 0; i < STAR_COUNT; i++) {
+			targetObject.position.copy(STARS[i].pos);
+			targetObject.updateMatrix();
+
+			star.setMatrixAt(i, targetObject.matrix);
+		}
+
+		scene.add(star);
+
+		/**
+		 * Helpers
+		 */
+
+		const axesHelp = new AxesHelper();
+		scene.add(axesHelp);
+
+		/**
+		 * Pane
+		 */
+
+		/**
+		 * Events
+		 */
+
+		const clock = new Clock();
+		let previousTime = 0;
+
+		let currentSpeed = 0.1;
+		let acceleration = 0.1;
+		let lerpSpeed = 0.005;
+
+		const updateObject = new Object3D();
+
+		function render(time?: number) {
+			requestAnimationFrame(render);
+
+			stats.update();
+			controls.update(time);
+
+			composer.render();
+
+			const elapsedTime = clock.getElapsedTime();
+			const deltaTime = elapsedTime - previousTime;
+			previousTime = elapsedTime;
+
+			// Won't reach 0.5 but close
+			currentSpeed += (acceleration - currentSpeed) * lerpSpeed;
+
+			star.instanceMatrix.needsUpdate = true;
+			for (let i = 0; i < STAR_COUNT; i++) {
+				if (STARS[i].pos.x > 3.0) {
+					STARS[i] = resetStar();
+				}
+
+				STARS[i].pos.x += STARS[i].speed * deltaTime * currentSpeed;
+				updateObject.position.copy(STARS[i].pos);
+
+				updateObject.updateMatrix();
+
+				star.setMatrixAt(i, updateObject.matrix);
+			}
+		}
+		render();
+
+		function resize() {
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			composer.setSize(window.innerWidth, window.innerHeight);
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+		}
+		window.addEventListener('resize', resize);
+
+		function accelerate() {
+			acceleration = 0.5;
+		}
+		function decelerate() {
+			acceleration = 0.1;
+		}
+		window.addEventListener('pointerdown', accelerate);
+		window.addEventListener('pointerup', decelerate);
+	};
+
+	useEffect(() => {
+		initialScene();
+	}, []);
+
+	return <div id='container' className={classes['container']}></div>;
 }
-
-export default App
